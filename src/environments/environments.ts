009bd59e16f6b1f29f121a395c38134af872dfc8
Jonas Clausen <jonastclausen@gmail.com>
2024-01-12 18:18:06 -0600

    ups production vars

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  //production: true,
-  // baseUrl: 'https://nest-fixsell-backend-production.up.railway.app',
-  production: false,
-  baseUrl: 'http://localhost:3000',
+  production: true,
+  baseUrl: 'https://nest-fixsell-backend-production.up.railway.app',
+  // production: false,
+  // baseUrl: 'http://localhost:3000',
 };

27a15ed2e478d0e1ef302259444a1a411b0cebaf
Jonas Clausen <jonastclausen@gmail.com>
2024-03-11 17:37:53 -0600

    clean up of old user crud

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardLayoutComponent } from './layouts/dashboard-layout/dashboard-layout.component';
 import { IntroScreenComponent } from './components/intro-screen/intro-screen.component';
-import { UsersComponent } from './components/users/users.component';
-import { UsersTableComponent } from './components/users-create/users-table.component';
-import { UserEditComponent } from './components/user-edit/user-edit.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { RoleGuard } from '../auth/guards/role.guard';
 
@@ -20,18 +17,6 @@ const routes: Routes = [
       //   canActivate: [RoleGuard],
       //   data: { allowedRoles: ['admin'] },
       // },
-      {
-        path: 'users-table',
-        component: UsersTableComponent,
-        canActivate: [RoleGuard],
-        data: { allowedRoles: ['admin'] },
-      },
-      {
-        path: 'users-edit',
-        component: UserEditComponent,
-        canActivate: [RoleGuard],
-        data: { allowedRoles: ['admin'] },
-      },
       { path: 'settings', component: SettingsComponent },
     ],
   },

02c3f2f994a09564a09e4494563236766aba4891
Jonas Clausen <jonastclausen@gmail.com>
2024-11-13 17:42:46 -0600

    Add event title display and update deal interface to include event data

--- src/app/website/interfaces/deal.interface.ts
+++ src/app/website/interfaces/deal.interface.ts
@@ -1,5 +1,6 @@
 import { Consumible } from './consumibles.interface';
 import { Printer } from './printer.interface';
+import { EventData } from './event.interface';
 
 export interface Deal {
   id: string;
@@ -12,4 +13,5 @@ export interface Deal {
   dealDiscountPercentage: number;
   dealDescription: string;
   eventId?: string; // Add this field to associate with an event
+  event?: EventData;
 }
